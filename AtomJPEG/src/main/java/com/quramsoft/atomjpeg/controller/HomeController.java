0ef1ff8d168446de5f5a3fec7f30bb569b988a71
Aziz Hasanov <aziz.hasanov.kh@gmail.com>
2017-12-04 19:43:46 +0900

    added new views (not working yet)

--- AtomJPEG/src/main/java/com/quramsoft/atomjpeg/controller/HomeController.java
+++ AtomJPEG/src/main/java/com/quramsoft/atomjpeg/controller/HomeController.java
@@ -76,9 +76,58 @@ public class HomeController {
 		System.out.println("Configured port: " + SERVER_PORT);
 		System.out.println("User home path: " + USER_HOME);
 
-		return "home";
+		model.addAttribute("searchinput", new SearchInput());
+
+		// if new request delete the files of original and compressed images
+
+		File org = new File(ORIGINAL_IMAGE_PATH);
+		File atom = new File(ATOM_IMAGE_PATH);
+		// File org_cached = new File(cached_original_image_path);
+		// File atom_cached = new File(cached_compressed_image_path);
+
+		File[] org_listOfFiles = org.listFiles();
+		File[] atom_listOfFiles = atom.listFiles();
+		// File[] org_cached_listOfFiles = org_cached.listFiles();
+		// File[] atom_cached_listOfFiles = atom_cached.listFiles();
+
+		if (org.isDirectory() && atom.isDirectory()) {
+			for (int i = 0; i < org_listOfFiles.length; i++) {
+				if (org_listOfFiles[i].isFile()) {
+					System.out.println("File " + org_listOfFiles[i].getName() + "is getting deleted");
+					org_listOfFiles[i].delete();
+				}
+			}
+			for (int i = 0; i < atom_listOfFiles.length; i++) {
+				if (atom_listOfFiles[i].isFile()) {
+					System.out.println("File " + atom_listOfFiles[i].getName() + "is getting deleted");
+					atom_listOfFiles[i].delete();
+				}
+			}
+		}
+
+		// if (org_cached.isDirectory() && atom_cached.isDirectory()) {
+		// for (int i = 0; i < org_cached_listOfFiles.length; i++) {
+		// if (org_cached_listOfFiles[i].isFile()) {
+		// System.out.println("File " + org_cached_listOfFiles[i].getName() + "is
+		// getting deleted");
+		// org_cached_listOfFiles[i].delete();
+		// }
+		// }
+		// for (int i = 0; i < atom_cached_listOfFiles.length; i++) {
+		// if (atom_cached_listOfFiles[i].isFile()) {
+		// System.out.println("File " + atom_cached_listOfFiles[i].getName() + "is
+		// getting deleted");
+		// atom_cached_listOfFiles[i].delete();
+		// }
+		// }
+
+		return "main"; // dynamic form using Spring's form tag library
+		
+		
+		//return "home";
 	}
 
+	/*
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String showForm(Model model) {
 
@@ -130,7 +179,7 @@ public class HomeController {
 		return "main"; // dynamic form using Spring's form tag library
 
 	}
-
+*/
 	
 	@RequestMapping(value = "/main", method = RequestMethod.POST)
 	public @ResponseBody ModelAndView resultForm(SearchInput searchInput) {
@@ -262,4 +311,134 @@ public class HomeController {
 
 	}
 
+	@RequestMapping(value = "/ready", method = RequestMethod.POST)
+	public @ResponseBody ModelAndView resultImages(SearchInput searchInput) {
+		ModelAndView mv = new ModelAndView("result");
+
+		int less_bandwidth = 0;
+		int average_size_reduction = 0;
+
+		ArrayList<String> listOfImageUrls = new ArrayList<String>();
+		listOfImageUrls = AtomCrawler.returnAllCrawledImageURLs(searchInput.getUrl(), searchInput.isJpg(),
+				searchInput.isPng(), searchInput.isGif());
+
+		System.out.println("STEP 1: All images URLs crawled! ");
+
+		
+		ArrayList<String> downloadedImageFinalNames = new ArrayList<String>();
+		downloadedImageFinalNames = AtomCrawler.downloadImages(listOfImageUrls);
+
+		System.out.println("STEP 2: All images downloaded! ");
+		
+		ArrayList<String> recompressedImageFinalNames = new ArrayList<String>();
+		recompressedImageFinalNames = AtomCrawler.recompressImages(downloadedImageFinalNames, searchInput.getProfile(),
+				searchInput.getLevel());
+
+		System.out.println("STEP 3: All images recompressed! ");
+
+		
+		
+		// MetaArray
+		ArrayList<ArrayList<String>> allImageResults = new ArrayList<ArrayList<String>>();		
+		for (int j = 0; j < recompressedImageFinalNames.size(); j++) {
+			
+			File file_compressed = new File(recompressedImageFinalNames.get(j));
+			File file_original = new File(downloadedImageFinalNames.get(j));
+			BufferedImage bimg = null;
+			int width = 0;
+			int height = 0;
+			try {
+				bimg = ImageIO.read(new File(downloadedImageFinalNames.get(j)));
+				width = bimg.getWidth();
+				height = bimg.getHeight();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+			double compressed_file_size = 0;
+			double original_file_size = 0;
+
+			if (file_compressed.exists()) {
+				compressed_file_size = file_compressed.length();// Double.toString(file_compressed_bytes);
+			} else {
+				System.out.println("the file" + recompressedImageFinalNames.get(j) + " doesnot exist");
+			}
+			if (file_original.exists()) {
+				original_file_size = file_original.length();// Double.toString(file_original.length()/1);
+			} else {
+				System.out.println("the file" + downloadedImageFinalNames.get(j) + " doesnot exist");
+			}
+
+			// --- Get resolution
+			Double compression_ratio = null;
+			try {
+				compression_ratio = 100 - ((compressed_file_size / original_file_size) * 100);// in percentile
+			} catch (ArithmeticException e) {
+				System.err.println("Caught IOException: " + e.getMessage());
+			}
+			
+			
+			String recompressImageLocForJsp;
+			
+			String[] temp_names_recompress = recompressedImageFinalNames.get(j).split("/");
+			recompressImageLocForJsp = "/" + temp_names_recompress[temp_names_recompress.length - 3];
+			recompressImageLocForJsp += "/" + temp_names_recompress[temp_names_recompress.length - 2];
+			recompressImageLocForJsp += "/" + temp_names_recompress[temp_names_recompress.length - 1];
+			
+			String originalImageLocForJsp;
+			
+			String[] temp_names_original = downloadedImageFinalNames.get(j).split("/");
+			originalImageLocForJsp = "/" + temp_names_original[temp_names_original.length - 3];
+			originalImageLocForJsp += "/" + temp_names_original[temp_names_original.length - 2];
+			originalImageLocForJsp += "/" + temp_names_original[temp_names_original.length - 1];
+			
+			ArrayList<String> ImageObject = new ArrayList<String>();
+			//ImageObject.add(listOfImageUrls.get(j));
+			ImageObject.add(originalImageLocForJsp);
+			//ImageObject.add(recompressedImageFinalNames.get(j));
+			ImageObject.add(recompressImageLocForJsp);
+			ImageObject.add(String.valueOf(original_file_size));
+			ImageObject.add(String.valueOf(compressed_file_size));
+			
+			less_bandwidth += original_file_size - compressed_file_size;
+			average_size_reduction += compression_ratio;
+			
+			ImageObject.add(String.valueOf(String.format("%.1f", compression_ratio)));
+			ImageObject.add(String.valueOf(width) + "x" + String.valueOf(height));
+			ImageObject.add(listOfImageUrls.get(j));
+			allImageResults.add(ImageObject);
+			System.out.println("############");
+			System.out.println("ImageObject " + j + ":" + ImageObject);			
+		}
+		
+		int size_reduction_percentage = average_size_reduction / downloadedImageFinalNames.size();
+		/*
+		 * for (int i = 0; i < downloadedImageFinalNames.size(); i++) {
+		 * ((Map<String,String>) allImageResults2).put(listOfImageUrls.get(i),
+		 * recompressedImageFinalNames1.get(i)); } //Awatiting deletion, let the judge
+		 * judge!
+		 */
+
+//		ArrayList<Integer> report = new ArrayList<Integer>();
+//		report.add(downloadedImageFinalNames.size());
+//		report.add(less_bandwidth);
+//		report.add(size_reduction_percentage);
+
+		//mv.addObject("report", report);
+		
+		mv.addObject("numberOfImages", downloadedImageFinalNames.size());
+		mv.addObject("less_bandwidth", less_bandwidth);
+		mv.addObject("size_reduction_percentage", size_reduction_percentage);
+		//mv.addObject("faster_load_time", faster_load_time);
+		//mv.addObject("annual_cdn_savings", annual_cdn_savings);
+		
+		mv.addObject("allImageResults", allImageResults);
+
+		//mv.addObject("listOfImageUrls", listOfImageUrls);
+		//mv.addObject("recompressedImageFinalNames1", recompressedImageFinalNames);
+		
+		return mv; // return model and view
+
+	}
+	
 }
---
+++ AtomJPEG/src/main/webapp/WEB-INF/views/ready.jsp
@@ -0,0 +1,126 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%-- the Spring Framework form tags can be used to bind a form to a model, 
+so that form can be auto-filled with model data. --%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="sf"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<html>
+<head>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>AtomJPEG Main Page </title>
+
+<!-- css files -->
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet prefetch"
+	href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+<link href="<c:url value="/resources/css/style.css" />"
+	rel="stylesheet">
+<!-- js files-->
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="//rawgit.com/saribe/eModal/master/dist/eModal.min.js"></script> 
+</head>
+<body>
+
+
+	<div class="container">
+		<div class="main_header">
+			<h1>AtomJpeg Test</h1>
+		</div>
+		<div class="user_input_form">
+			<form id="searchinput" action="/atomjpeg/ready" method="POST">
+				<div class="row">
+					<div class="col-md-8">
+						<input id="url" name="url"
+							placeholder="http://www.yourwebsite.com" type="text"
+							class="form-control searchinput" value="">
+					</div>
+					<div class="col-md-4">
+						<button class="btn-primary">Start</button>
+					</div>
+				</div>
+				<!-- End of first row-->
+
+				<!-- Start of 2nd row -->
+				<div class="row">
+					<div class="col-md-3 atom_option">
+						<fieldset>
+							<legend>Atom option</legend>
+						</fieldset>
+						<div class="row">
+							<div class="col-md-12">
+								<select class="form-control" name="profile" id="profile">
+
+									<option value="" style="background-color: black;">Profile</option>
+									<option value="">Fast</option>
+									<option value="Baseline">Baseline</option>
+									<option value="Main">Main</option>
+									<option value="High">High</option>
+								</select>
+							</div>
+
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<select class="form-control" name="level" id=""level"">
+									<option value="">Level</option>
+									<option value="High Quality">High Quality</option>
+									<option value="Normal">Normal</option>
+									<option value="High Compression">High Compression</option>
+									<option value="Extreme">Extreme</option>
+								</select>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-9">
+						<div class="row">
+							<div class="col-md-12">
+								<fieldset>
+									<legend>Image option</legend>
+									<div class="checkboxes">
+										<input id="jpg" name="jpg" type="checkbox"><span class="check_box">JPG</span>
+										<input id="png" name="png" type="checkbox"><span class="check_box">PNG</span>
+										<input id="gif" name="gif" type="checkbox"><span class="check_box">GIF</span>
+									</div>
+								</fieldset>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 network_option">
+								<fieldset>
+									<legend>Network option</legend>
+									Monthly PageView : <input id="pageview" name="pageview" type="text"> 
+									Billing per 1GB($): <input id="billing" name="billing" type="text">
+								</fieldset>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+
+
+	</div>
+
+
+
+
+
+
+	<!--  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js'></script>
+  -->
+
+
+
+	<script> eModal.alert('You shall not pass!');</script>
+
+</body>
